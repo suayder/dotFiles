c5bfc334b035add313beb555527730a5a3db2683
suayder <suayder@uft.edu.br>
2020-03-23 00:43:32 -0300

    basic settings, more is comming

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotFiles
 Dot files for arch linux settings
+
+to run setup your computer just run `setup.sh` inside the repository folder.
+
+Optionaly you can compy and replance your `~/Xresources` by the one avaliable here.
+
+Posteriorly I will setup the nexts developer programs.
---
+++ setup.sh
@@ -0,0 +1,38 @@
+ #!/bin/bash
+
+base_path=$PWD
+echo "Yay instalation..."
+cd /opt
+sudo git clone https://aur.archlinux.org/yay.git
+sudo chown -R $USER yay
+cd yay && makepkg -si
+
+cd $base_path
+echo "Installing base packages..."
+yay -S \
+  bash-completion
+
+echo "VScode..."
+yay -S visual-studio-code-bin -y
+
+echo "Installing tools..."
+yay -S \
+  feh \
+  acpi \
+  ttf-font-awesome-4 \
+  openssh \
+  arandr \
+  redshift \
+  lm_sensors \
+  python-pywal
+
+echo "Some applications..."
+yay -S \
+  spotify
+
+cp ./picom.conf ~/.conf/
+exec --no-startup-id picom --config ~/.config/picom.conf
+sed -i "\$abindsym \$mod+p exec \"wal -i /usr/share/backgrounds/\"" ~/.i3/config
+sed -i "\$a#auto start red-shift\nexec --no-startup-id redshift-gtk &" ~/.i3/config
+
+sed -i "\$afeh --bg-max --randomize /usr/share/backgrounds/* &" ~/.xinitrc
